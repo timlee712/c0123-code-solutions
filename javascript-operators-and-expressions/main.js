b0da4497483763255e5472ce92f41aae9d9e4d82
Timothy Lee <leetim1202@gmail.com>
2023-01-30 20:01:00 +0000

    javascript-operators-and-expressions Timothy Lee

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 500;
+var height = 500;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 1000;
+var payment = 500;
+var change = bill - payment;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 10;
+var midterm = 5;
+var final = 1;
+var grade = ((quizzes + midterm + final) / 3);
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Timothy ';
+var lastName = 'Lee';
+var fullName = firstName + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 8;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 15;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
