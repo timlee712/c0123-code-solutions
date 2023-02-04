5e76febae4cf0c2aa80dad31ae8d4a04069f93c6
Timothy Lee <leetim1202@gmail.com>
2023-02-04 19:38:58 +0000

    object-manipulation-1 Timothy Lee

---
+++ object-manipulation-1/src/get-keys.js
@@ -0,0 +1,10 @@
+/* exported getKeys */
+
+function getKeys(object) {
+  var keys = [];
+  for (var key in object) {
+    keys.push(key);
+
+  }
+  return keys;
+}
---
+++ object-manipulation-1/src/to-object.js
@@ -0,0 +1,5 @@
+/* exported toObject */
+
+function toObject(keyValuePair) {
+  return { [keyValuePair[0]]: keyValuePair[1] };
+}
