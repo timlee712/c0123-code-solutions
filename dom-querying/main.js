218888048eaccbf8f9344e59e673aca8d10ff76b
Timothy Lee <leetim1202@gmail.com>
2023-02-06 18:07:03 +0000

    dom-querying Timothy Lee

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,18 +1,18 @@
 
-document.querySelector('h1');
-console.log(document.querySelector('h1'));
-console.dir(document.querySelector('h1'));
+var $heading = document.querySelector('h1');
+console.log($heading);
+console.dir($heading);
 
-document.querySelector('#explanation');
-console.log(document.querySelector('#explanation'));
-console.dir(document.querySelector('#explanation'));
+var $explanation = document.querySelector('#explanation');
+console.log($explanation);
+console.dir($explanation);
 
-document.querySelector('.hint');
-console.log(document.querySelector('.hint'));
-console.dir(document.querySelector('.hint'));
+var $hint = document.querySelector('.hint');
+console.log($hint);
+console.dir($hint);
 
-document.querySelectorAll('p');
-console.log(document.querySelectorAll('p'));
+var $paragraphs = document.querySelectorAll('p');
+console.log(document.querySelectorAll($paragraphs));
 
-document.querySelectorAll('.example-link');
-console.log(document.querySelectorAll('.example-link'));
+var $links = document.querySelectorAll('.example-link');
+console.log(document.querySelectorAll($links));
