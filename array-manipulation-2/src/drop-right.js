7cae97001cdaeb6b1b7cc241b3b80a0b43ce3d8c
Timothy Lee <leetim1202@gmail.com>
2023-02-07 21:52:14 +0000

    array-manipulation-2 Timothy Lee

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,9 @@
 /* exported dropRight */
+
+function dropRight(array, count) {
+  var droppedRight = [];
+  for (var i = 0; i < array.length - count; i++) {
+    droppedRight.push(array[i]);
+  }
+  return droppedRight;
+}
