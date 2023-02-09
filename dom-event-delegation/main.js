46c30be2be296c66a6b9293e1dbfffc375819d24
Timothy Lee <leetim1202@gmail.com>
2023-02-09 22:46:46 +0000

    dom-event-delegation Timothy Lee

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -3,7 +3,6 @@ $taskList.addEventListener('click', function (event) {
   console.log('event.target:', event.target);
   console.log('event.target.tagName', event.target.tagName);
 
-  // i can also use event.target.tagName !== 'BUTTON' instead. event.target.nodeName is a bit outdated
   if (event.target && event.target.nodeName === 'BUTTON') {
     var $closest = event.target.closest('.task-list-item');
     console.log('closest .task-list-item:', $closest);
