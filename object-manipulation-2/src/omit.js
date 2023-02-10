46a5c7d16eb7e8057538b41625c3eaacaa037907
Timothy Lee <leetim1202@gmail.com>
2023-02-10 20:10:36 +0000

    object-manipulation-2 Timothy Lee

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,7 @@
 /* exported omit */
+
+function omit(source, keys) {
+  var output = {};
+
+  return output;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,4 +1,11 @@
 /* exported pick */
-function pick() {
-
+function pick(source, keys) {
+  var output = {};
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    if (source[key] !== undefined) {
+      output[key] = source[key];
+    }
+  }
+  return output;
 }
