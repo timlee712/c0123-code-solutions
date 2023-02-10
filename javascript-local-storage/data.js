48ee44eeeac5452908c188d2bd2ad5b533e041c1
Timothy Lee <leetim1202@gmail.com>
2023-02-10 19:29:50 +0000

    javascript-local-storage Timothy Lee

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,16 @@
+/* exported todos */
+
+var todos = [];
+
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+
+});
