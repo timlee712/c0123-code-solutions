4c2216a6a0c7919033a5e6bfc44c6145de53f22f
Timothy Lee <leetim1202@gmail.com>
2023-02-20 18:42:01 +0000

    javascript-this Timothy Lee

---
+++ javascript-this/student.js
@@ -0,0 +1,13 @@
+/* exported student */
+
+var student = {
+  firstName: 'Timothy',
+  lastName: 'Lee',
+  subject: 'Javascript',
+  getFullName: function () {
+    return this.firstName + ' ' + this.lastName;
+  },
+  getIntroduction: function () {
+    return 'Hello, my name is ' + this.getFullName() + ' and I am studying ' + this.subject + '.';
+  }
+};
