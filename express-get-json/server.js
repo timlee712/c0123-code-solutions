130442f90c845cac3f463d3d4110f5467f1652f6
Timothy Lee <leetim1202@gmail.com>
2023-03-21 00:05:04 +0000

    Completed express-get-json - Timothy Lee

---
+++ express-get-json/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+const app = express();
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray = [];
+  for (const key in grades) {
+    gradesArray.push(grades[key]);
+  }
+  res.json(gradesArray);
+});
+
+app.listen(8080, () => {
+  console.log('Server listening on port 8080');
+});
