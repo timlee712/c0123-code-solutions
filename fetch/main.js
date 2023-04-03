df3243e633b5d4d8fb57724227fb8c89ac074dbf
Timothy Lee <leetim1202@gmail.com>
2023-04-03 23:46:58 +0000

    Added users array from the users api

--- fetch/main.js
+++ fetch/main.js
@@ -1,3 +1,17 @@
+async function fetchUsers() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response) {
+      throw new Error(`error: ${response.status} Failed to fetch data from API.`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+}
+fetchUsers();
+
 async function fetchPokemonData(pokemonId) {
   const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
   try {
