1fde79356320c91a132165c0afbb5956d7cfeaf9
Timothy Lee <leetim1202@gmail.com>
2023-04-03 23:58:22 +0000

    Added .ok to res

--- fetch/main.js
+++ fetch/main.js
@@ -1,10 +1,10 @@
 async function fetchUsers() {
   try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    if (!response) {
-      throw new Error(`error: ${response.status} Failed to fetch data from API.`);
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!res.ok) {
+      throw new Error(`error: ${res.status} Failed to fetch data from API.`);
     }
-    const data = await response.json();
+    const data = await res.json();
     console.log(data);
   } catch (err) {
     console.error(err);
@@ -15,11 +15,11 @@ fetchUsers();
 async function fetchPokemonData(pokemonId) {
   const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
   try {
-    const response = await fetch(apiUrl);
-    if (!response) {
-      throw new Error(`error: ${response.status} Failed to fetch data from API.`);
+    const res = await fetch(apiUrl);
+    if (!res.ok) {
+      throw new Error(`error: ${res.status} Failed to fetch data from API.`);
     }
-    const data = await response.json();
+    const data = await res.json();
     console.log(data);
   } catch (err) {
     console.error(err);
